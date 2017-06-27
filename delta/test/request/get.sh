a2132fd55ea6a3a0dd81015fafc60fce4dc47e3a
SolarWarden <damienmonaghan@hotmail.com>
2017-06-27 15:40:28 +0100

    some updates

---
+++ delta/test/request/get.sh
@@ -0,0 +1,7 @@
+curl -vX GET http://localhost:3000/$1/  \
+-H "Content-Type: application/json" \
+-H "authorization: myauthkey" \
+-H "cid: 12345" \
+-H "pn: wip" \
+-H "pv: 1.0.0" \
+-H "rid: 987654321" 
